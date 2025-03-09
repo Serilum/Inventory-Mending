76c37da44e925fcba3243dc330568886efd4ee5a
Rick South <rick@natamus.com>
2025-03-09 11:23:18 +0100

    (v1.2) Lower mixin priority to fix compatibility issue with the Clumps mod.

--- Common/src/main/java/com/natamus/inventorymending/util/Reference.java
+++ Common/src/main/java/com/natamus/inventorymending/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.inventorymending.util;
 public class Reference {
 	public static final String MOD_ID = "inventorymending";
 	public static final String NAME = "Inventory Mending";
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.2";
 	public static final String ACCEPTED_VERSIONS = "[1.21.4]";
 }
