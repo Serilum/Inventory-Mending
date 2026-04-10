1ff15551041f2fe5d877a8f97c7175d6517f40ed
Rick South <rick@natamus.com>
2026-04-10 02:52:04 +0200

    Inventory Mending source code for 26.1.2.

---
+++ Common/src/main/java/com/natamus/inventorymending/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.inventorymending;
+
+import com.natamus.inventorymending.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
---
+++ Common/src/main/java/com/natamus/inventorymending/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.inventorymending.util;
+
+public class Reference {
+	public static final String MOD_ID = "inventorymending";
+	public static final String NAME = "Inventory Mending";
+	public static final String VERSION = "1.2";
+	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+}
